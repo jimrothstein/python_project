ef6bbbd5a7b1ea23d398e742384b62d71531dee9
jimrothstein <jimrothstein@gmail.com>
2026-06-11 16:31:34 -0700

    wip

--- 310_opencode_python.py
+++ 310_opencode_python.py
@@ -1,11 +1,14 @@
+# WORKS
 from openai import OpenAI
+import os
 
 # use my openapi key, already displays in opencode/zen, just copy
 # works for some of the zen models
 # 
 #  
 client = OpenAI(
-    base_url="https://opencode.ai/zen/v1"
+    base_url="https://opencode.ai/zen/v1",
+    api_key=os.getenv("OPENCODE_API_KEY")
 )
 
 resp = client.chat.completions.create(
